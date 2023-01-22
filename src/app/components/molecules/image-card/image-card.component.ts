29cb7e903f180e4b338ce86187ca35a7dc9d41f8
Jaima Jarnaz <jaima.jarnaz1234@gmail.com>
2023-01-21 19:00:16 -0800

    fix input

--- src/app/components/molecules/image-card/image-card.component.ts
+++ src/app/components/molecules/image-card/image-card.component.ts
@@ -6,13 +6,9 @@ import { Component, HostBinding, Input, OnInit } from '@angular/core';
   styleUrls: ['./image-card.component.scss'],
 })
 export class ImageCardComponent implements OnInit {
-  @Input()
-  imgSrc!: string;
-
-  @Input()
-  title!: string;
-  @Input()
-  description!: string;
+  @Input() imgSrc!: string;
+  @Input() title: string = ''; // Required value so assign as empty
+  @Input() description!: string;
   @Input() alt?: string;
   @HostBinding('class') classes: string = 'm-image-card';
 
